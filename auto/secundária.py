3af8a155ad43c4e714a9e0091b1decbb414f666e
Rafael Alves <javdt.rapf@gmail.com>
2025-11-02 10:03:08 -0300

    auto

---
+++ auto/secundária.py
@@ -0,0 +1,11 @@
+import pyautogui
+import time
+import pandas as pd
+
+dataframe = pd.read_csv("D:/Python/Intensivo Python/Automação/produtos.csv")
+
+print(dataframe)
+
+#while True:
+    #time.sleep(4)
+    #print (pyautogui.position())
